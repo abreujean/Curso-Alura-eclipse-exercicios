17a2376e8f9d8df9fe7b5ea7bc927ff2946c8590
Jean Abreu <30299701+jeabreu@users.noreply.github.com>
2019-03-05 23:21:14 -0300

    Adicionando exercícios

---
+++ java-pilha/src/TestaContaComExcecaoCheked.java
@@ -0,0 +1,16 @@
+
+public class TestaContaComExcecaoCheked {
+
+	public static void main(String[] args) {
+		
+
+		Conta c = new Conta();
+		
+		try {
+			c.deposita();
+		} catch (MinhaExcecao ex) {
+			System.out.println("tratemento...");
+		}
+	}
+
+}
